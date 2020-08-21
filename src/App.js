cd777391ca76c4c91b4a0fa45eaae5c0d7468a72
Andrew Ruiz <nukedoom@gmail.com>
2020-08-21 10:49:41 -0700

    Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import Form from './Form'
+
+// React Router Import
+import { BrowserRouter as Router, BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { Route, Link } from 'react-router-dom';
+
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+
+    <BrowserRouter>
+    <div>
+      <Route exact path='/'>
+        <Link to='/pizza'>
+        <button>Form</button>
+        </Link>
+      </Route>
+
+  
+      <Route path='/pizza'>
+        <Form />
+
+      </Route>
+    </div>
+
+    </BrowserRouter>
   );
 };
+
 export default App;
